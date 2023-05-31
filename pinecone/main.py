05df0a0139f3d2e830389cdb3f01fef5a5141d3d
Willy Douhard <willy.douhard@gmail.com>
2023-05-31 21:24:51 +0200

    remove dead code and add comments

--- pinecone/main.py
+++ pinecone/main.py
@@ -1,5 +1,4 @@
 import os
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Pinecone
 from langchain.chains import RetrievalQAWithSourcesChain
@@ -12,13 +11,13 @@ pinecone.init(
     environment=os.environ.get("PINECONE_ENV"),
 )
 
-index_name = "test"
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
-embeddings = OpenAIEmbeddings()
 
 index_name = "langchain-demo"
+
 # Optional
 namespace = None
+
+embeddings = OpenAIEmbeddings()
 
 welcome_message = "Welcome to the Chainlit Pinecone demo! Ask anything about documents you vectorized and stored in your Pinecone DB."
 
@@ -48,25 +47,33 @@ def process_response(res):
 
     if docs:
         metadatas = [doc.metadata for doc in docs]
+        # Get the source names from the metadata
         all_sources = [m["source"] for m in metadatas]
 
         if sources:
             found_sources = []
-
+            # For each source mentioned by the LLM
             for source_index, source in enumerate(sources.split(",")):
+                # Remove the period and any whitespace
                 orig_source_name = source.strip().replace(".", "")
+                # The name that will be displayed in the UI
                 clean_source_name = f"source {source_index}"
                 try:
+                    # Find the mentioned source in the list of all sources
                     found_index = all_sources.index(orig_source_name)
                 except ValueError:
                     continue
+                # Get the text from the source document
                 text = docs[found_index].page_content
+
                 found_sources.append(clean_source_name)
                 source_elements.append(cl.Text(text=text, name=clean_source_name))
 
             if found_sources:
+                # Add the sources to the answer, referencing the text elements
                 answer += f"\nSources: {', '.join(found_sources)}"
             else:
                 answer += "\nNo sources found"
 
+    # Send the answer and the text elements to the UI
     cl.Message(content=answer, elements=source_elements).send()
