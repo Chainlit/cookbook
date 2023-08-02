4cccdf9c6f1ae10f6b228644af794e4981b6cca7
Willy Douhard <willy.douhard@gmail.com>
2023-08-02 11:56:02 +0200

    update anthropic example

--- anthropic-chat/app.py
+++ anthropic-chat/app.py
@@ -1,20 +1,20 @@
-
 import os
 import anthropic
-import json
-import ast
 import chainlit as cl
 
-c = anthropic.Client(os.environ["ANTHROPIC_API_KEY"])
+c = anthropic.AsyncAnthropic(api_key=os.environ["ANTHROPIC_API_KEY"])
 
 
 @cl.on_chat_start
 async def start_chat():
     cl.user_session.set(
-        "prompt_history", "",
+        "prompt_history",
+        "",
     )
-    await cl.Avatar(name="Anthropic", 
-                    url="https://pbs.twimg.com/profile_images/1398287823229251584/FVs58Hks_400x400.jpg").send()
+    await cl.Avatar(
+        name="Anthropic",
+        url="https://pbs.twimg.com/profile_images/1398287823229251584/FVs58Hks_400x400.jpg",
+    ).send()
 
 
 @cl.on_message
@@ -23,23 +23,19 @@ async def chat(message: str):
 
     prompt = f"{prompt_history}{anthropic.HUMAN_PROMPT}{message}{anthropic.AI_PROMPT}"
 
-    ui_msg = cl.Message(
-        author="Anthropic",
-        content="",
-        prompt=prompt
-    )
+    ui_msg = cl.Message(author="Anthropic", content="")
 
-    response = await c.acompletion_stream(
+    stream = await c.completions.create(
         prompt=prompt,
         stop_sequences=[anthropic.HUMAN_PROMPT],
         max_tokens_to_sample=1000,
-        model="claude-2.0", # claude-instant-1.1
+        model="claude-2.0",  # claude-instant-1.1
         stream=True,
     )
 
-    async for data in response:
-        token = data['completion']
-        await ui_msg.stream_token(token, is_sequence=True)
+    async for data in stream:
+        token = data.completion
+        await ui_msg.stream_token(token)
 
     await ui_msg.send()
 
