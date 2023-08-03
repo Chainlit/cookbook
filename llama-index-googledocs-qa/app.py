645f3733e14c62ec3041b6a8f470469e11668336
Dan Constantini <constantinidan@gmail.com>
2023-08-03 10:08:27 +0200

    fix quoting

--- llama-index-googledocs-qa/app.py
+++ llama-index-googledocs-qa/app.py
@@ -28,7 +28,7 @@ def load_context():
         # Storage not found; create a new one
         GoogleDocsReader = download_loader('GoogleDocsReader')
 
-        gdoc_ids = ['G-DOC-ID-1', 'G-DOC-ID-2', 'G-DOC-ID-3]
+        gdoc_ids = ['G-DOC-ID-1', 'G-DOC-ID-2', 'G-DOC-ID-3']
 
         loader = GoogleDocsReader()
         documents = loader.load_data(document_ids=gdoc_ids)
