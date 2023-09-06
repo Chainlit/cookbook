9a84ce6a72cdcd8e833de6e098cb848bf8ad5739
Willy Douhard <willy.douhard@gmail.com>
2023-09-06 19:44:50 +0200

    add local llms examples

---
+++ local-llm/llama-cpp.py
@@ -0,0 +1,74 @@
+from langchain.llms import LlamaCpp
+from langchain import PromptTemplate
+from langchain.chains import ConversationChain
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+
+import chainlit as cl
+from chainlit.playground.config import add_llm_provider
+from chainlit.playground.providers.langchain import LangchainGenericProvider
+
+
+# Download the file here https://huggingface.co/TheBloke/Yarn-Llama-2-7B-128K-GGUF/tree/main
+# and update the path
+MODEL_PATH = "/Users/willydouhard/Downloads/yarn-llama-2-7b-128k.Q3_K_M.gguf"
+
+
+@cl.cache
+def instantiate_llm():
+    n_batch = (
+        4096  # Should be between 1 and n_ctx, consider the amount of VRAM in your GPU.
+    )
+    # Make sure the model path is correct for your system!
+    llm = LlamaCpp(
+        model_path=MODEL_PATH,
+        n_batch=n_batch,
+        n_ctx=4096,
+        temperature=1,
+        max_tokens=10000,
+        n_threads=64,
+        verbose=True,  # Verbose is required to pass to the callback manager
+        streaming=True,
+    )
+    return llm
+
+
+llm = instantiate_llm()
+
+add_llm_provider(
+    LangchainGenericProvider(id=llm._llm_type, name="Llama-cpp", llm=llm, is_chat=False)
+)
+
+
+@cl.on_chat_start
+def main():
+    template = """### System Prompt
+The following is a friendly conversation between a human and an AI optimized to generate source-code. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know:
+
+### Current conversation:
+{history}
+
+### User Message
+{input}
+
+### Assistant"""
+
+    prompt = PromptTemplate(template=template, input_variables=["history", "input"])
+
+    conversation = ConversationChain(
+        prompt=prompt, llm=llm, memory=ConversationBufferWindowMemory(k=10)
+    )
+
+    cl.user_session.set("conv_chain", conversation)
+
+
+@cl.on_message
+async def main(message: str):
+    conversation = cl.user_session.get("conv_chain")
+
+    cb = cl.LangchainCallbackHandler(
+        stream_final_answer=True, answer_prefix_tokens=["Assistant"]
+    )
+
+    cb.answer_reached = True
+
+    res = await cl.make_async(conversation)(message, callbacks=[cb])
---
+++ local-llm/llama2-chat.py
@@ -0,0 +1,79 @@
+from langchain.llms import HuggingFacePipeline
+from transformers import AutoTokenizer, TextStreamer
+from langchain import PromptTemplate, LLMChain
+import transformers
+import torch
+
+import chainlit as cl
+from chainlit.playground.config import add_llm_provider
+from chainlit.playground.providers.langchain import LangchainGenericProvider
+
+
+template = """
+You are a helpful AI assistant. Provide the answer for the following question:
+
+Question: {question}
+Answer:
+"""
+
+# You need to be approved by HF & Meta https://huggingface.co/meta-llama/Llama-2-7b-chat-hf/tree/main
+
+
+@cl.cache
+def load_llama():
+    model = "meta-llama/Llama-2-7b-chat-hf"
+    tokenizer = AutoTokenizer.from_pretrained(model)
+    streamer = TextStreamer(tokenizer, skip_prompt=True)
+    pipeline = transformers.pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        torch_dtype=torch.float16,
+        trust_remote_code=True,
+        device_map="auto",
+        max_length=1000,
+        do_sample=True,
+        top_k=10,
+        num_return_sequences=1,
+        eos_token_id=tokenizer.eos_token_id,
+        streamer=streamer,
+        use_auth_token=True,
+    )
+
+    llm = HuggingFacePipeline(
+        pipeline=pipeline,
+        model_kwargs={"temperature": 0},
+    )
+    return llm
+
+
+llm = load_llama()
+
+add_llm_provider(
+    LangchainGenericProvider(
+        id=llm._llm_type, name="Llama2-chat", llm=llm, is_chat=False
+    )
+)
+
+
+@cl.on_chat_start
+async def main():
+    prompt = PromptTemplate(template=template, input_variables=["question"])
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+    cl.user_session.set("llm_chain", llm_chain)
+
+    return llm_chain
+
+
+@cl.on_message
+async def run(input_str):
+    cb = cl.AsyncLangchainCallbackHandler(
+        stream_final_answer=True, answer_prefix_tokens=["Answer"]
+    )
+
+    # Retrieve the chain from the user session
+    llm_chain = cl.user_session.get("llm_chain")  # type: LLMChain
+    res = await llm_chain.acall(input_str, callbacks=[cb])
+
+    if not cb.answer_reached:
+        await cl.Message(content=res["text"]).send()
