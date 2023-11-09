3656edfeeea89169d56c3b2fa80b043d31e71849
Willy Douhard <willy.douhard@gmail.com>
2023-11-09 13:42:03 +0100

    add support for latext and images

--- openai-assistant/app.py
+++ openai-assistant/app.py
@@ -1,11 +1,39 @@
 import os
 
 from openai import AsyncOpenAI
+from openai.types.beta.threads import (
+    MessageContentImageFile,
+    MessageContentText,
+    ThreadMessage,
+)
 import chainlit as cl
 
 api_key = os.environ.get("OPENAI_API_KEY")
 client = AsyncOpenAI(api_key=api_key)
 assistant_id = os.environ.get("ASSISTANT_ID")
+
+
+async def process_thread_message(thread_message: ThreadMessage):
+    for content_message in thread_message.content:
+        if isinstance(content_message, MessageContentText):
+            await cl.Message(
+                author=thread_message.role, content=content_message.text.value
+            ).send()
+        elif isinstance(content_message, MessageContentImageFile):
+            image_id = content_message.image_file.file_id
+            response = await client.files.with_raw_response.retrieve_content(image_id)
+            await cl.Message(
+                author=thread_message.role,
+                content="",
+                elements=[
+                    cl.Image(
+                        name=image_id,
+                        content=response.content,
+                        display="inline",
+                        size="large",
+                    ),
+                ],
+            ).send()
 
 
 @cl.on_chat_start
@@ -19,7 +47,7 @@ async def start_chat():
 async def run_conversation(message_from_ui: cl.Message):
     thread = cl.user_session.get("thread")
     # Add the message to the thread
-    oai_message = await client.beta.threads.messages.create(
+    init_message = await client.beta.threads.messages.create(
         thread_id=thread.id, role="user", content=message_from_ui.content
     )
 
@@ -29,8 +57,8 @@ async def run_conversation(message_from_ui: cl.Message):
     )
 
     # Send empty message to display the loader
-    response_msg = cl.Message(author="assistant", content="")
-    await response_msg.send()
+    loader_msg = cl.Message(author="assistant", content="")
+    await loader_msg.send()
 
     running = True
 
@@ -56,12 +84,11 @@ async def run_conversation(message_from_ui: cl.Message):
         step_details = run_step.step_details
         # Send step content to the Chainlit UI
         if step_details.type == "message_creation":
-            message = await client.beta.threads.messages.retrieve(
+            thread_message = await client.beta.threads.messages.retrieve(
                 message_id=step_details.message_creation.message_id,
                 thread_id=thread.id,
             )
-            response_msg.content = message.content[0].text.value
-            await response_msg.update()
+            await process_thread_message(thread_message)
         if step_details.type == "tool_calls":
             for tool_call in step_details.tool_calls:
                 if tool_call.type == "code_interpreter":
@@ -82,3 +109,5 @@ async def run_conversation(message_from_ui: cl.Message):
                         content="Retrieving information",
                         parent_id=message_from_ui.id,
                     ).send()
+
+    await loader_msg.remove()
--- openai-assistant/create_assistant.py
+++ openai-assistant/create_assistant.py
@@ -13,9 +13,15 @@ client = AsyncOpenAI(api_key=api_key)
 
 
 async def create():
+    instructions = """You are a personal math tutor. Write and run code to answer math questions.
+Reply in markdown, especially for math expressions using the $$ expression.
+
+Example:
+Given a formula below $$ s = ut + \frac{1}{2}at^{2} $$ Calculate the value of $s$ when $u = 10\frac{m}{s}$ and $a = 2\frac{m}{s^{2}}$ at $t = 1s$
+"""
     assistant = await client.beta.assistants.create(
         name="Math Tutor",
-        instructions="You are a personal math tutor. Write and run code to answer math questions.",
+        instructions=instructions,
         tools=[{"type": "code_interpreter"}],
         model="gpt-4-1106-preview",
     )
