2f6c2b243e5abcf86734c92895fc1f2fda1b967a
Willy Douhard <willy.douhard@gmail.com>
2024-01-13 11:45:53 +0100

    use a better example

--- literal-langserve/chainlit-app/app.py
+++ literal-langserve/chainlit-app/app.py
@@ -3,20 +3,24 @@ from langserve import RemoteRunnable
 import chainlit as cl
 
 
-@cl.step(type="run")
-async def invoke_runnable(input: str):
-    test_chain = RemoteRunnable(
+def get_runnable():
+    runnable = RemoteRunnable(
         "http://localhost:8001/test/",
         headers={
             "thread_id": cl.context.session.thread_id,
-            "run_id": cl.context.current_step.id,
         },
     )
 
-    return await test_chain.ainvoke(input)
+    return runnable
 
 
 @cl.on_message
 async def on_msg(msg: cl.Message):
-    response = await invoke_runnable(msg.content)
-    await cl.Message(content=f"Retrieved {len(response)} documents!").send()
+    msg = cl.Message(content="")
+
+    async for chunk in get_runnable().astream(
+        {"question": msg.content},
+    ):
+        await msg.stream_token(chunk)
+
+    await msg.send()
--- literal-langserve/langserve-app/app/server.py
+++ literal-langserve/langserve-app/app/server.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 """Example LangChain server exposes a retriever."""
 from fastapi import FastAPI
-from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores.faiss import FAISS
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema import StrOutputParser
 
 import uuid
 
@@ -18,17 +19,22 @@ from langserve import add_routes
 
 client = LiteralClient()
 
-vectorstore = FAISS.from_texts(
-    ["cats like fish", "dogs like sticks"], embedding=OpenAIEmbeddings()
+model = ChatOpenAI(streaming=True)
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You're a very knowledgeable historian who provides accurate and eloquent answers to historical questions.",
+        ),
+        ("human", "{question}"),
+    ]
 )
-retriever = vectorstore.as_retriever()
+runnable = prompt | model | StrOutputParser()
 
 
 def per_req_config_modifier(config, request):
     thread_id = request.headers.get("thread_id") or uuid.uuid4().hex
     parent_id = request.headers.get("run_id")
-
-    print("current thread", active_steps_var.get())
 
     active_thread_var.set(Thread(id=thread_id))
     if parent_id:
@@ -51,7 +57,7 @@ app = FastAPI(
 # /stream
 add_routes(
     app,
-    retriever,
+    runnable,
     config_keys=["callbacks"],
     per_req_config_modifier=per_req_config_modifier,
     path="/test",
