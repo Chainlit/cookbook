0eaf7b69efa040ba7edc409210977a005dc1e245
modenter <paul.de-la-porte-des-vaux@epita.fr>
2024-09-12 17:08:02 +0200

    fix: fixed copilot hello world

--- copilot/app.py
+++ copilot/app.py
@@ -3,4 +3,4 @@ import chainlit as cl
 
 @cl.on_message
 async def on_message(msg: cl.Message):
-    return cl.Message(content="Hello, world!")
+    return await cl.Message(content="Hello, world!").send()
