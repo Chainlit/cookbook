a2c5b1461f37513d9079d4f8f1ac7a7e208dd3e5
jfperusse-bhvr <Jean-Francois.Perusse@bhvr.com>
2024-10-31 16:28:05 -0400

    Add a sample of two-way window messaging

---
+++ window-message/app.py
@@ -0,0 +1,12 @@
+import chainlit as cl
+
+@cl.on_window_message
+async def window_message(message: str):
+    if message.startswith("Client: "):
+        await cl.Message(content=f"Window message received: {message}").send()
+        cl.send_window_message(f"Server: Window message received: {message}")
+
+@cl.on_message
+async def on_message(msg: cl.Message):
+    await cl.Message(content=f"Normal message received: {msg.content}").send()
+    cl.send_window_message(f"Server: Normal message received: {msg.content}")
